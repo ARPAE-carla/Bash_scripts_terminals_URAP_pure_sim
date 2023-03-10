8b3a7be60dbc6ac7414cb0603930d185941cf494
shn66 <phillipe1042003@gmail.com>
2023-03-10 15:43:40 -0800

    Fixed a spelling error with catkin_ws and also added delays between each execution of a bash script

--- entire_terminals.sh
+++ entire_terminals.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Bash script to run all other bash scripts
 
-gnome-terminal --tab -- bash -c "./carla_setup.sh ; ./pure_sim.sh ; ./ros2_connect_ws.sh ; ./ros1_bridge_ws.sh ; ./ctkin_ws.sh ; exec bash"
+gnome-terminal --tab -- bash -c "./carla_setup.sh ; sleep 1 ; ./pure_sim.sh ; sleep 1 ; ./ros2_connect_ws.sh ; sleep 1 ; ./ros1_bridge_ws.sh ; sleep 1 ; ./catkin_ws.sh"
 

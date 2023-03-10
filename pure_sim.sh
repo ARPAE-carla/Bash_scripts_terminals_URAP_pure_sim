580434225a2b8286097d2e5e98550c5be9e1dba5
shn66 <phillipe1042003@gmail.com>
2023-03-10 15:46:43 -0800

    Missed running a couple nodes in pure_sim.sh

--- pure_sim.sh
+++ pure_sim.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # Bash Script to run the Nuvo mpc controller nodes
 
-source /opt/ros/eloquent/setup.bash
-source ~/Documents/pure_sim_URAP/pure_sim_ws/install/setup.bash
 
-gnome-terminal --tab -- bash -c "cd ~/Documents/pure_sim_URAP/pure_sim_ws ; ros2 run mpclab_controllers_arpae nuvo_ros2_mpc_with_planner.py ; exec bash"
+
+gnome-terminal --tab -- bash -c "cd ~/Documents/pure_sim_URAP/pure_sim_ws ; source /opt/ros/eloquent/setup.bash ; source ~/Documents/pure_sim_URAP/pure_sim_ws/install/setup.bash ; ros2 run mpclab_controllers_arpae nuvo_ros2_mpc_with_planner.py ; exec bash"
+gnome-terminal --tab -- bash -c "cd ~/Documents/pure_sim_URAP/pure_sim_ws ; source /opt/ros/eloquent/setup.bash ; source ~/Documents/pure_sim_URAP/pure_sim_ws/install/setup.bash ; ros2 run mpclab_controllers_arpae nuvo_ros2_planner.py ; exec bash"
+gnome-terminal --tab -- bash -c "cd ~/Documents/pure_sim_URAP/pure_sim_ws ; source /opt/ros/eloquent/setup.bash ; source ~/Documents/pure_sim_URAP/pure_sim_ws/install/setup.bash ; ros2 run mpclab_controllers_arpae simulate_model.py ; exec bash"

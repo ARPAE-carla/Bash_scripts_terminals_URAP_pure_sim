0cecd7f709277c526c4abbbd3cbcbc09bdd31b33
shn66 <phillipe1042003@gmail.com>
2023-03-14 16:14:01 -0700

    Changed up the order of execution to put pure_sim and catkin tabs next to each other. Also increased delay to allow for Carla setup

--- entire_terminals.sh
+++ entire_terminals.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Bash script to run all other bash scripts
 
-gnome-terminal --tab -- bash -c "./carla_setup.sh ; sleep 1 ; ./pure_sim.sh ; sleep 1 ; ./ros2_connect_ws.sh ; sleep 1 ; ./ros1_bridge_ws.sh ; sleep 1 ; ./catkin_ws.sh"
+gnome-terminal --tab -- bash -c "./carla_setup.sh ; sleep 10 ; ./ros2_connect_ws.sh ; ./ros1_bridge_ws.sh ; ./pure_sim.sh ; ./catkin_ws.sh"
 

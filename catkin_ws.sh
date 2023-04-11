ac08812a65a285e174bc22f0c72090f042338989
shn66 <phillipe1042003@gmail.com>
2023-04-10 18:32:31 -0700

    Updated the bash script for starting the ROS1 code: no longer want to automatically start ros_carla_test.py at the start of the simulation because cosimulation works now

--- catkin_ws.sh
+++ catkin_ws.sh
@@ -2,4 +2,10 @@
 # Bash script to run the ros_carla_test only (for now)
 
 gnome-terminal --tab -- bash -c "cd ~/Documents/ROs1-URAP/catkin_ws ; source /opt/ros/melodic/setup.bash ; roscore ; exec bash"
-gnome-terminal --tab -- bash -c "cd ~/Documents/ROS1-URAP/catkin_ws ; sleep 5 ; source /opt/ros/melodic/setup.bash ; source ~/Documents/ROS1-URAP/catkin_ws/devel/setup.bash ; rosrun ros_carla_sumo_integration ros_carla_test.py ; exec bash"
+
+# For running the original pure simulation with Carla Traffic Manager handling NPCs
+# gnome-terminal --tab -- bash -c "cd ~/Documents/ROS1-URAP/catkin_ws ; sleep 5 ; source /opt/ros/melodic/setup.bash ; source ~/Documents/ROS1-URAP/catkin_ws/devel/setup.bash ; rosrun ros_carla_sumo_integration ros_carla_test.py ; exec bash"
+
+# For running the pure simulation with Sumo handling NPCs
+# Since there are so many inputs, will just type out
+gnome-terminal --tab -- bash -c "cd ~/Documents/ROS1-URAP/catkin_ws ; sleep 5 ; source /opt/ros/melodic/setup.bash ; source ~/Documents/ROS1-URAP/catkin_ws/devel/setup.bash ; exec bash"
